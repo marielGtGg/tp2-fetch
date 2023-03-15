33d2d86677c12f00c482d8ddda6dccdb7d5c091d
Mariel Gauthier-Grégoire <mariel.gauthier.gregoire@gmail.com>
2023-03-14 22:30:40 -0400

    image is changing

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
 const btn = document.getElementById('next-dog-button');
+const img = document.getElementById('dogsAPI');
 
 btn.addEventListener('click', () => {
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(data => {
-        console.log(data.message);
-    })
-    .catch(error => {
-        console.log('Erreur : ' + error)
-    })   
+    .then(data => img.src = data.message)
+    .catch(error => console.log('Erreur : ' + error))   
 });
