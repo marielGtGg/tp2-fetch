b9bf57d4f839b63f7c3c2273d7c9cd7889a3d3d5
Mariel Gauthier-Grégoire <mariel.gauthier.gregoire@gmail.com>
2023-03-14 22:25:48 -0400

    added eventListener and fetch

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
+const btn = document.getElementById('next-dog-button');
+
+btn.addEventListener('click', () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.message);
+    })   
+});
