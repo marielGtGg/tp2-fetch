06dc40b7f9dda1920497135721b129fce783108b
Mariel Gauthier-Grégoire <mariel.gauthier.gregoire@gmail.com>
2023-03-14 22:49:43 -0400

    added random detail info

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
 const btn = document.getElementById('next-dog-button');
 const img = document.getElementById('dogsAPI');
+const id = document.getElementById('dogId');
+const age = document.getElementById('dogAge');
+const registry = document.getElementById('dogRegistry');
 
 btn.addEventListener('click', () => {
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(data => img.src = data.message)
+    .then(data => {
+        img.src = data.message;
+        id.textContent = randomNumber(10000, 99999);
+        age.textContent = randomNumber(1, 15) + ' Years';
+        registry.textContent = randomNumber(100, 999);
+    })
     .catch(error => console.log('Erreur : ' + error))   
 });
+
+function randomNumber(min, max) {
+    return min + Math.floor((max - min + 1) * Math.random());
+}
